49453d34b8df27d5e6f5031d04a9b738a3c6b5d7
Jay Rome Rana <itaalyyy@gmail.com>
2024-10-04 12:30:47 +0800

    webhook debug

--- index.cjs
+++ index.cjs
@@ -276,6 +276,8 @@ app.post('/webhook', async (req, res) => {
 	const payment_intent_id = req.body.data.attributes.payment_intent_id;
 	const payment_id = req.body.data.id;
 
+	console.log(type, payment_intent_id, payment_id);
+
 	if (type == 'payment.paid') {
 		const searchQuery =
 			'SELECT user_id FROM payment_intents WHERE payment_intent_id = $1';
@@ -283,26 +285,37 @@ app.post('/webhook', async (req, res) => {
 
 		try {
 			const searchResult = await client.query(searchQuery, searchValues);
+			console.log(searchResult);
+
 			const user_id = searchResult.rows[0].user_id;
 
 			const query =
 				'INSERT INTO payments (user_id, payment_intent_id, payment_id) VALUES ($1, $2, $3)';
 			const values = [user_id, payment_intent_id, payment_id];
+
 			await client.query(query, values);
+
+			console.log(user_id);
 
 			console.log('Payment successful!');
 			res.status(200).json({ message: 'Payment successful!' });
+
+			return;
 		} catch (error) {
 			console.log('Payment error!');
 			res.status(500).json({ error: error.stack });
+
+			return;
 		}
 	} else if (type == 'payment.failed') {
 		console.log('Payment failed!');
 		res.status(409).json({ message: 'Payment failed!' });
+
+		return;
 	}
 
-	console.log('Invalid event type!');
-	res.status(400).json({ message: 'Invalid event type!' });
+	console.log('Unknown error occurred!');
+	res.status(500).json({ message: 'Unknown error occurred!' });
 });
 
 /// RUN                                                                                                                        ///
