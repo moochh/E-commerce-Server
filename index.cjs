99b6989992522cdb3ba5ae6fa54b9af4904f0fab
Jay Rome Rana <itaalyyy@gmail.com>
2024-10-04 23:54:15 +0800

    add favorites methods

--- index.cjs
+++ index.cjs
@@ -251,6 +251,20 @@ app.delete('/cart/:user_id', async (req, res) => {
 });
 
 /// FAVORITES                                                                                                                  ///
+//> Get
+app.get('/favorites/:user_id', async (req, res) => {
+	const { user_id } = req.params;
+	const query = 'SELECT * FROM favorites WHERE user_id = $1';
+	const values = [user_id];
+
+	try {
+		const result = await client.query(query, values);
+		res.status(200).json(result.rows);
+	} catch (error) {
+		res.status(500).json({ error: error.stack });
+	}
+});
+
 //> Add
 app.post('/favorites/:user_id', async (req, res) => {
 	const { user_id } = req.params;
@@ -267,6 +281,28 @@ app.post('/favorites/:user_id', async (req, res) => {
 		await client.query(query, values);
 
 		res.status(201).json({ message: 'Product added to favorites!' });
+	} catch (error) {
+		res.status(500).json({ error: error.stack });
+	}
+});
+
+//> Remove
+app.delete('/favorites/:user_id', async (req, res) => {
+	const { user_id } = req.params;
+	const { product_id } = req.body;
+
+	if (!user_id || !product_id) {
+		return res.status(400).json({ error: 'Missing required fields!' });
+	}
+
+	try {
+		const query =
+			'DELETE FROM favorites WHERE user_id = $1 AND product_id = $2';
+		const values = [user_id, product_id];
+
+		await client.query(query, values);
+
+		res.status(200).json({ message: 'Product removed from favorites!' });
 	} catch (error) {
 		res.status(500).json({ error: error.stack });
 	}
