c7bcc6f2a0577fa39ed555d1187731ec5f5e2a0a
Jay Rome Rana <itaalyyy@gmail.com>
2024-10-04 11:56:03 +0800

    add payment intents endpoint

--- index.cjs
+++ index.cjs
@@ -249,6 +249,27 @@ app.delete('/cart/:user_id', async (req, res) => {
 	}
 });
 
+/// PAYMENT INTENTS                                                                                                            ///
+app.post('/payment-intents', async (req, res) => {
+	const { user_id, payment_intent_id } = req.body;
+
+	if (!user_id || !payment_intent_id) {
+		return res.status(400).json({ error: 'Missing required fields!' });
+	}
+
+	try {
+		const query =
+			'INSERT INTO payment_intents (user_id, payment_intent_id) VALUES ($1, $2)';
+		const values = [user_id, payment_intent_id];
+
+		await client.query(query, values);
+
+		res.status(201).json({ message: 'Payment intent created!' });
+	} catch (error) {
+		res.status(500).json({ error: error.stack });
+	}
+});
+
 /// WEBHOOK                                                                                                                    ///
 app.post('/webhook', async (req, res) => {
 	const type = req.body.data.attributes.type;
