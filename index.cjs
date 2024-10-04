afce91631d2428cb7aa8b3adff56f53d37d45381
Jay Rome Rana <itaalyyy@gmail.com>
2024-10-04 12:22:39 +0800

    fix webhook db

--- index.cjs
+++ index.cjs
@@ -273,21 +273,27 @@ app.post('/payment-intents', async (req, res) => {
 /// WEBHOOK                                                                                                                    ///
 app.post('/webhook', async (req, res) => {
 	const type = req.body.data.attributes.type;
-	console.log(req.body.data.attributes);
+	const payment_intent_id = req.body.data.attributes.payment_intent_id;
+	const payment_id = req.body.data.id;
 
 	if (type == 'payment.paid') {
-		const query = 'INSERT INTO payments (name) VALUES ($1)';
-		const values = ['kim clores'];
+		const searchQuery =
+			'SELECT user_id FROM payment_intents WHERE payment_intent_id = $1';
+		const searchValues = [payment_intent_id];
 
 		try {
+			const searchResult = await client.query(searchQuery, searchValues);
+			const user_id = searchResult.rows[0].user_id;
+
+			const query =
+				'INSERT INTO payments (user_id, payment_intent_id, payment_id) VALUES ($1, $2, $3)';
+			const values = [user_id, payment_intent_id, payment_id];
 			await client.query(query, values);
+
+			res.status(200).json({ message: 'Payment successful!' });
 		} catch (error) {
 			res.status(500).json({ error: error.stack });
 		}
-
-		res.status(200).json({ message: 'Payment successful!' });
-
-		return;
 	} else if (type == 'payment.failed') {
 		res.status(409).json({ message: 'Payment failed!' });
 	}
