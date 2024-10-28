391655a0c5cfde7d32c04eb3c43ce6241f63ba5f
Jay Rome Rana <itaalyyy@gmail.com>
2024-10-29 01:33:33 +0800

    add admin id

--- routes/adminRoutes.cjs
+++ routes/adminRoutes.cjs
@@ -4,14 +4,17 @@ const { client } = require('../services.cjs');
 
 const adminAccounts = [
 	{
+		id: 1,
 		email: 'admin1@email.com',
 		password: 'admin1pass'
 	},
 	{
+		id: 2,
 		email: 'admin2@email.com',
 		password: 'admin2pass'
 	},
 	{
+		id: 3,
 		email: 'admin3@email.com',
 		password: 'admin3pass'
 	}
@@ -29,7 +32,10 @@ router.post('/admin-login', async (req, res) => {
 		(account) => account.email === email && account.password === password
 	);
 
-	if (admin) return res.status(200).json({ message: 'Login successful!' });
+	if (admin)
+		return res
+			.status(200)
+			.json({ message: 'Login successful!', admin_id: admin.id });
 	else return res.status(401).json({ error: 'Invalid email or password' });
 });
 
