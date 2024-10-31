2e3e07d02fb5f45d08c754af24fb3abc735b3ead
Jay Rome Rana <itaalyyy@gmail.com>
2024-10-31 10:51:36 +0800

    add update billing

--- routes/billingRoutes.cjs
+++ routes/billingRoutes.cjs
@@ -89,6 +89,56 @@ router.post('/billing/:user_id', async (req, res) => {
 	}
 });
 
+//> Update
+router.put('/billing/:user_id/:billing_id', async (req, res) => {
+	const { user_id, billing_id } = req.params;
+	const {
+		first_name,
+		last_name,
+		email,
+		phone_number,
+		address_line_1,
+		address_line_2,
+		city,
+		postal_code
+	} = req.body;
+
+	if (
+		!first_name ||
+		!last_name ||
+		!email ||
+		!phone_number ||
+		!address_line_1 ||
+		!address_line_2 ||
+		!city ||
+		!postal_code
+	)
+		res.status(400).json({ error: 'Missing required fields!' });
+
+	const query =
+		'UPDATE billing_address SET first_name = $1, last_name = $2, email = $3, phone_number = $4, address_line_1 = $5, address_line_2 = $6, city = $7, postal_code = $8 WHERE user_id = $9 AND id = $10';
+	const values = [
+		first_name,
+		last_name,
+		email,
+		phone_number,
+		address_line_1,
+		address_line_2,
+		city,
+		postal_code,u
+		user_id,
+		billing_id
+	];
+
+	try {
+		const result = await client.query(query, values);
+
+		res.status(200).json({ message: 'Billing address updated successfully!' });
+	} catch (error) {
+		res.status(500).json({ error: error.stack });
+	}
+});
+
 //> Delete
 router.delete('/billing/:user_id/:billing_id', async (req, res) => {
 	const { user_id, billing_id } = req.params;
